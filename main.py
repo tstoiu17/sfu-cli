b456116eade3f8928f7120ec567146e6116994fc
Tudor <tudor.stoiu@gmail.com>
2022-08-28 19:22:22 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import requests
+import json
+import os
+
+
+def display_options(options):
+    """Print the options dictionary with styling using ANSI escape codes"""
+    escape_code = '\u001b[{}m'
+    fg_red_bold = escape_code.format('31;1')
+    fg_blue_bold = escape_code.format('34;1')
+    reset = escape_code.format('0')
+
+    for k, v in options.items():
+        if k == 'q' or k == 'r':
+            print(fg_red_bold, end='')
+        else:
+            print(fg_blue_bold, end='')
+        print(f'({k}) {v}')
+        print(reset, end='')
+
+
+base_url = f'http://www.sfu.ca/bin/wcm/course-outlines?'
+req_url = base_url
+params = []
+level = 0
+quit = False
+while level < 5 and not quit:
+    os.system('clear')
+    query = "/".join(params)
+    req_url = f'{base_url}{query}'
+    r = requests.get(req_url)
+    print(f'{level}: Requesting /{query}')
+    j = json.loads(r.text)
+    # display options
+    options = {str(i): obj['text'] for i, obj in enumerate(j)}
+    # TODO: include these options for choosing year and term
+    """
+    # dyanmic variable parameters
+    options['c'] = 'current'
+    options['r'] = 'registration'
+    """
+    options['q'] = 'quit'
+    options['r'] = 'restart'
+    display_options(options)
+    # prompt for choice
+    invalid = True
+    while invalid:
+        choice = input('> ').strip()
+        level += 1
+
+        # check if choice is valid
+        if choice in options:
+            invalid = False
+            if choice == 'q':
+                quit = True
+            if choice == 'r':
+                params.clear()
+                level = 0
+            else: # update params with choice
+                params.append(j[int(choice)]['value'])
+        else:
+            print('Invalid choice entered')
+
